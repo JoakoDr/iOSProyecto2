5a322b9c5b2d0dc63640719b1e5f7b66cb533b2a
JoaquinDiaz <joaco_diaz_ramirez@hotmail.com>
2018-04-05 07:16:19 -0700

    Hito1 Completo

---
+++ AppPueblos/CVCCollection.swift
@@ -0,0 +1,13 @@
+//
+//  CVCCollection.swift
+//  AppPueblos
+//
+//  Created by JOAQUIN DIAZ RAMIREZ on 5/4/18.
+//  Copyright © 2018 JOAQUIN DIAZ RAMIREZ. All rights reserved.
+//
+
+import UIKit
+
+class CVCCollection: UICollectionViewCell {
+    
+}
