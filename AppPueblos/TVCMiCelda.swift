2b6a2ff90a158ee3ed0592e6deddf3017cd3e147
JoaquinDiaz <joaco_diaz_ramirez@hotmail.com>
2018-04-05 01:37:18 -0700

    Tabla terminada, con mapa incluido

---
+++ AppPueblos/TVCMiCelda.swift
@@ -0,0 +1,24 @@
+//
+//  TVCMiCelda.swift
+//  AppPueblos
+//
+//  Created by JOAQUIN DIAZ RAMIREZ on 5/4/18.
+//  Copyright © 2018 JOAQUIN DIAZ RAMIREZ. All rights reserved.
+//
+
+import UIKit
+
+class TVCMiCelda: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
